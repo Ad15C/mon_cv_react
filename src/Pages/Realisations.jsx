9b8dcb7ee1ac5dff5fd6478c4043f20331831035
Ad15C <ad15canon@hotmail.com>
2024-07-29 18:44:36 +0200

    router

---
+++ src/Pages/Realisations.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Realisations = () => {
+    return (
+        <main className="container">
+            <h1>A Propos</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </main>
+    );
+}
+
+export default Realisations;
